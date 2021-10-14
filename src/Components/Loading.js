cfd1e45b4dfcbb321e1f708d871c1154edbe8263
evanmcpheron <evan.mcpheron@icloud.com>
2021-10-14 11:59:09 -0700

    added custom class to Loading

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const Loading = () => {
+const Loading = ({ customClass }) => {
   return (
-    <div data-test={"loading-component"} className='loading-container'>
-      <svg
-        className="loader"
-        viewBox="0 0 120 120"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+    <div
+      data-test={'loading-component'}
+      className={`loading-container ${customClass === undefined ? '' : customClass}`}
+    >
+      <svg className="loader" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
         <circle className="internal-circle" cx="60" cy="60" r="30"></circle>
         <circle className="external-circle" cx="60" cy="60" r="50"></circle>
       </svg>
